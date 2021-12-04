36589a7e8a4622e3f5e19c327f92e22e43aa919c
Zsolt <kzsolt3141@gmail.com>
2021-12-04 14:21:31 +0200

    Integrate UART 1.0.6

--- test/main.c
+++ test/main.c
@@ -12,6 +12,29 @@
 #include "uart.h"
 #include "timer0.h"
 
+/**
+ * USART RX interrupt callback handle context
+ */
+struct USART_RXC_cb_ctx_t {
+    uint8_t rx;  // received byte to be seved here
+};
+
+/**
+ * USART RX interrupt callback handle
+ * @param[inout] ctx user data for interrupt callback
+ * When ISR occurs USART_RXC_cb will be called with ctx as parameter
+ * UART RX data (UDR) should be saved in this function
+ */
+static void USART_RXC_cb_handle(void* ctx) {
+    struct USART_RXC_cb_ctx_t* t_ctx = (struct USART_RXC_cb_ctx_t*)ctx;
+
+    t_ctx->rx = UDR;
+    printf("%c\n", t_ctx->rx);
+}
+
+/**
+ * TIMER interrupt callback handle context
+ */
 struct Timer0_OVF_cb_ctx_t {
     uint8_t tmr0_init_val;
     int tmr0_int_cnt;
@@ -26,16 +49,36 @@ static void TIMER0_OVF_cb_handle(void* ctx) {
 
 int main(void)
 {
-    struct Timer0_OVF_cb_ctx_t timer0_ctx = {0};
-    timer0_ctx.tmr0_init_val = 0x64;
+    // UART INIT
+    //-------------------------------
+    const uint16_t baud_rate = 38400;
 
-    USARTInit();
-    TIMER0Init(0x64);
+    uint8_t sts = 0;
+    struct USART_RXC_cb_ctx_t USART_RXC_ctx = {};
+
+    USART_init(baud_rate);
+
+    sts = regiter_USART_RXC_cb(USART_RXC_cb_handle, &USART_RXC_ctx);
+    if (sts) return sts;
+
+    printf("Init Done UART baud: %u\n", (uint16_t)baud_rate);
+    //-------------------------------
+
+    // TIMER0 init
+    //-------------------------------
+    const uint8_t tmr0_init_val = 0x64;
+    struct Timer0_OVF_cb_ctx_t timer0_ctx = {0};
+    timer0_ctx.tmr0_init_val = tmr0_init_val;
+
+    TIMER0Init(tmr0_init_val);
 
     regiter_TIMER0_OVF_cb(TIMER0_OVF_cb_handle, &timer0_ctx);
+    //-------------------------------
+
     // TIMER1Init(0xE17C);
     // TIMER1CompareInit(15625);
     // TIMER1PWMInit();
+
     while (1) {
         printf("%d \n", timer0_ctx.tmr0_int_cnt);
         _delay_ms(1000);
